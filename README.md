71a5eaaf56ebdf0b7236e5a8f670562278e22c47
Yan Jiang <yanjiang@YandeMacBook-Pro.local>
2019-02-09 00:00:35 +0800

    docs

--- README.md
+++ README.md
@@ -1,50 +1,24 @@
-## web develope tooltik
+# Moe
 
-此处需要一篇博客解释
+An ultra-lightweight, dependency-free api development framework without any third-partydependencies
+[中文文档](readme_zh.md)
 
-## todo
-
-ctx上挂一堆res和req里我常用到的简写方法
-
-考虑一下要不要保留state,
-
-把别人的巧克力融化,倒进自己模子里
-
-##todo:
-
-文档 ❓
-
-c.Next() ✔️
-
-Controller起个名字
-
-handlerFunc是因为别人有handler这个概念，去掉了handler这一层，我们这个直接叫handlerFun有点突兀
-用Handler, 有http.Handler的概念在前，会被弄混，所以也不合适
-Controller像是当年一大波宣扬自己是MVC模式的框架搞出来的东西，叫这个有点过时
-我自己瞎起的话就叫Poi好了（逃
-
-配置文件
-
-method拦截
-
-测试
-
-写的越多, bug越多, 能少写就少写
-
-c.Assert()需要重新思考一下，err!=nil到底算true还是false, 类型匹配的完吗，要不要用反射？
-
-c.Required(), 参数种类需要提前验证减少麻烦，创业那个表单的灵感
-纸上谈兵闷头造轮子行不通的，哪怕试用范围再小也不能只靠脑补，实践出真知
-
-JSONRPC了解一下？只能完全匹配的话干嘛不大胆一点，path只写两样信息，版本和action,直接往函数名上精确匹配
+## Getting Started
 
 
-一开始肯定想搞一个支持普遍意义上RESTfulAPI的框架
-因为不想用第三方，啥都得自己写
-理想的功能列一下
-1. 最基本的，完全路径匹配
-2. 高级一点就是defaultServeMux像那样， 匹配最长的
-3. 另一种思路是加一个*通配符来匹配
-4. 匹配带参数的路径
-5. defaultServeMux居然连request.method匹配都不支持，这个一定要有
-想办法往类似jsonRPC那样改造，
+## Usage
+
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+## License
+
+[MIT](LICENSE.md)
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
--- context.go
+++ context.go
@@ -3,119 +3,77 @@ package moe
 import (
 	"container/list"
 	"encoding/json"
+	"log"
 	"net/http"
+)
+
+const (
+	JSONEncodeError = "JSON Encode Error"
 )
 
 // Context is
 type Context struct {
 	middlewareList *list.List
-	Writer         http.ResponseWriter
-	Method         string
-	Path           string
+	writer         http.ResponseWriter
+
+	Method        string
+	Path          string
+	UserAgent     string
+	Authorization string
 }
 
 // newContext returns a new Context instance for the given res and req
 func newContext(w http.ResponseWriter, r *http.Request) *Context {
 	return &Context{
-		Writer: w,
-		Method: r.Method,
-		Path:   r.URL.Path,
+		writer: w,
+
+		Method:        r.Method,
+		Path:          r.URL.Path,
+		UserAgent:     r.Header.Get("User-Agent"),
+		Authorization: r.Header.Get("Authorization"),
 	}
 }
 
-// // Set is used to store a new key/value pair exclusively for this context.
-// // use golang context
-// func (c *Context) Set(key, value interface{}) *Context {
-// 	c.Request.WithContext(context.WithValue(c.Request.Context(), key, value))
-// 	return c
-// }
-
-// // Get returns the value for the given key
-// // use golang context
-// func (c *Context) Get(key interface{}) interface{} {
-// 	return c.Request.Context().Value(key)
-// }
-
-// Method returns the http request method as string
-// ex: "GET"
-// func (c *Context) Method() string {
-// 	return c.Request.Method
-// }
-
-// Path is
-func (c *Context) Path() string {
-	return c.Request.URL.Path
-}
-
-// Header returns the header for the given key
-func (c *Context) Header(key string) string {
-	return c.Request.Header.Get(key)
-}
-
-// UserAgent is a shortcut for c.Header("User-Agent")
-func (c *Context) UserAgent() string {
-	return c.Header("User-Agent")
-}
-
-// Authorization is a shortcut for c.Header("Authorization")
-func (c *Context) Authorization() string {
-	return c.Header("Authorization")
-}
-
-// ParseForm is
-func (c *Context) ParseForm() *Context {
-	return c.Assert(c.Request.ParseForm(), http.StatusBadRequest, "")
-}
-
-// Form is a wrapper for req.Form.Get()
-func (c *Context) Form(key string) string {
-	return c.Request.Form.Get(key)
-}
-
-// Status is a wrapper for c.Writer.WriteHeader
+// setStatus is a wrapper for c.Writer.WriteHeader
 // The provided code must be a valid HTTP 1xx-5xx status code
 func (c *Context) Status(code int) *Context {
-	c.Writer.WriteHeader(code)
+	c.writer.WriteHeader(code)
 	return c
 }
 
-// ContentType is a shortcut for c.Writer.Header().Set("Content-Type", value)
+// setContentType is a wrapper for c.Writer.Header().Set("Content-Type", value)
 func (c *Context) ContentType(value string) *Context {
-	c.Writer.Header().Set("Content-Type", value)
-	return c
-}
-
-// Write is an alias for c.res.Write(body)
-func (c *Context) Write(body []byte) *Context {
-	c.Writer.Write(body)
+	c.writer.Header().Set("Content-Type", value)
 	return c
 }
 
 // JSON convert some value to JSON, and write them to response
-func (c *Context) JSON(code int, body interface{}) *Context {
-	err := json.NewEncoder(c.Writer).Encode(body)
-	return c.
-		Status(code).
-		ContentType("application/json").
-		Assert(err, 500, "JSON Encode Error")
+func (c *Context) Send(code int, obj interface{}) *Context {
+	c.ContentType("application/json")
+	err := json.NewEncoder(c.writer).Encode(obj)
+	if err != nil {
+		log.Println(err)
+		return c.Error(500, JSONEncodeError)
+	}
+	return c.Status(code)
 }
 
-// Assert is similar to .Error(), it Errors when:
-// statement(error type) != nil or
-// statement(bool) == false`
-func (c *Context) Assert(assertion interface{}, code int, msg string) *Context {
-	switch a := assertion.(type) {
-	case error:
-		if a != nil {
-			return c.Error(code, msg)
-		}
-	case bool:
-		if !a {
-			return c.Error(code, msg)
-		}
-	}
-	return c
-}
+// // Assert is similar to .Error(), it Errors when:
+// // statement(error type) != nil or
+// // statement(bool) == false`
+// func (c *Context) Assert(assertion interface{}, code int, msg string) *Context {
+// 	switch a := assertion.(type) {
+// 	case error:
+// 		if a != nil {
+// 			return c.Error(code, msg)
+// 		}
+// 	case bool:
+// 		if !a {
+// 			return c.Error(code, msg)
+// 		}
+// 	}
+// 	return c
+// }
 
 // Error to formart Error message
 type Error struct {
@@ -125,7 +83,7 @@ type Error struct {
 // Throw is like http.Error(), convert error msg to json form
 func (c *Context) Error(code int, msg string) *Context {
 	c.Status(code)
-	if err := json.NewEncoder(c.Writer).Encode(&Error{msg}); err != nil {
+	if err := json.NewEncoder(c.writer).Encode(&Error{msg}); err != nil {
 		http.Error(c.Writer, msg, code)
 	}
 	return c
