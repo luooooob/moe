2aade7b83a97b8e13e836e5c094ee52bb24c4b5e
luob5 <luob5@outlook.com>
2019-02-11 00:19:28 +0800

    create request

--- request.go
+++ request.go
@@ -2,34 +2,85 @@ package moe
 
 import (
 	"encoding/json"
-	"io"
 	"net/http"
+	"strings"
 )
+
+// // Request is
+// type Request struct {
+// 	http.Request
+// 	Method     string
+// 	Header     http.Header
+// 	URL        *url.URL
+// 	Path       string
+// 	Origin     string
+// 	IP         string
+// 	Type       string
+// 	Fresh      bool
+// 	Secure     bool
+// 	idempotent bool
+// }
 
 // Request is
 type Request struct {
-	Method string
-	Path   string
-	Header http.Header
-	body   io.Reader
+	http.Request
+	Path       string
+	Origin     string
+	IP         string
+	Type       string
+	Fresh      bool
+	Secure     bool
+	Idempotent bool
 }
 
 // newContext returns a new Context instance for the given res and req
 func newRequest(r *http.Request) *Request {
-	return &Request{
-		Method: r.Method,
-		Path:   r.URL.Path,
-		Header: r.Header,
-		body:   r.Body,
-	}
-}
-
-// Parse is
-func (req *Request) Parse(v interface{}) error {
-	return json.NewDecoder(req.body).Decode(v)
+	req := &Request{Request: *r}
+	req.Path = req.URL.Path
+	req.Origin = req.Get("Origin")
+	req.Type = req.Get("Content-Type")
+	req.IP = req.getClientIP()
+	req.Fresh = req.isFresh()
+	req.Idempotent = req.isIdempotent()
+	return req
 }
 
 // Get is
 func (req *Request) Get(key string) string {
 	return req.Header.Get(key)
+}
+
+// Parse is
+func (req *Request) Parse(v interface{}) error {
+	return json.NewDecoder(req.Body).Decode(v)
+}
+
+// Query is todo
+func (req *Request) Query(key string) string {
+	return ""
+}
+
+// getClientIP is
+func (req *Request) getClientIP() string {
+	ipString := req.Get("X-Forwarded-For")
+	if ipString != "" {
+		IPs := strings.Split(ipString, ",")
+		return IPs[0]
+	}
+	return req.RemoteAddr
+}
+
+// isSecure is
+func (req *Request) isSecure() bool {
+	return req.TLS != nil
+}
+
+// isFresh is  todo
+func (req *Request) isFresh() bool {
+	return false
+}
+
+// isIdempotent is todo
+func (req *Request) isIdempotent() bool {
+	return false
 }
